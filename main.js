e9e2b96099858d619a61b4a56ce28db3984b2cec
Alexander <bolotovalexander57@gmail.com>
2019-12-25 01:44:58 +0200

    Still in process

---
+++ main.js
@@ -0,0 +1,8 @@
+//Test only//
+window.onload = () => {
+    function ku() {
+        let popform = document.querySelector('pop-up-write-us-form');
+        console.info(popform.style.display );
+        popform.style.display = "none";
+       }
+    }
